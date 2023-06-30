cd8fd9555af88b0cf4955f2c50a9d6a8505a3468
Siddharthchakravarthy <84708034+Siddharthchakravarthy@users.noreply.github.com>
2023-06-30 11:57:43 +0530

    Add files via upload

---
+++ DefectAge/src/DateComparator.java
@@ -0,0 +1,101 @@
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class DateComparator {
+	public static String filePath;
+	public void PrintCurrentDate(InputStream fileContent) {   
+				LocalDate currentDate = LocalDate.now();
+	            filePath = "C:\\Defect File\\DefectAgeing"+currentDate+".xlsx";
+	            try (
+	            		Workbook workbook = WorkbookFactory.create(fileContent)) {
+	                Sheet sheet = workbook.getSheetAt(2); // Assuming you're working with the first sheet
+
+	                for (Row row : sheet) {
+	                	if(row.getRowNum() == 0) {
+	                		Cell cell1 = row.createCell(14);
+	                		cell1.setCellValue("Current Date");
+	                		continue;
+	                	}
+	                	if(row.getRowNum() == sheet.getLastRowNum()) {
+	                		continue;
+	                	}
+	                	CellStyle cellStyle = workbook.createCellStyle();
+	                	CreationHelper createHelper = workbook.getCreationHelper();
+	                	cellStyle.setDataFormat(
+	                	    createHelper.createDataFormat().getFormat("dd-mm-yy"));
+	                	Cell cell = row.createCell(14);
+	                	cell.setCellValue(new Date());
+	                	cell.setCellStyle(cellStyle);
+	                }
+		               
+//	                // Save the modified workbook
+	                try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+	                    workbook.write(outputStream);
+	                }
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	
+	public void getDateDifference() {
+		LocalDate currentDate = LocalDate.now();
+        filePath = "C:\\Defect File\\DefectAgeing"+currentDate+".xlsx";
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream(filePath))) {
+            Sheet sheet = workbook.getSheetAt(2); // Assuming you're working with the first sheet
+
+            for (Row row : sheet) {
+            	if(row.getRowNum() == 0) {
+            		Cell cell1 = row.createCell(15);
+            		cell1.setCellValue("Date Difference");
+            		continue;
+            	}
+                Cell date1Cell = row.getCell(2); // Column 2
+                Cell date2Cell = row.getCell(14); // Column 14
+                Cell dateDiffCell = row.createCell(15); // Column 15
+
+                if (date1Cell != null && date1Cell.getCellType() == CellType.NUMERIC &&
+                      date2Cell != null && date2Cell.getCellType() == CellType.NUMERIC) {
+                	  double date1Value = date1Cell.getNumericCellValue();
+                      double date2Value = date2Cell.getNumericCellValue();
+
+                      java.util.Date date1 = DateUtil.getJavaDate(date1Value);
+                      java.util.Date date2 = DateUtil.getJavaDate(date2Value);
+
+                      LocalDate localDate1 = Instant.ofEpochMilli(date1.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+                      LocalDate localDate2 = Instant.ofEpochMilli(date2.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+                    
+                      long daysDiff = ChronoUnit.DAYS.between(localDate1, localDate2);
+
+                    dateDiffCell.setCellValue(daysDiff);
+                }
+            }
+
+            // Save the modified workbook
+            try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+                workbook.write(outputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ DefectAge/src/ExcelColumnFilter.java
@@ -0,0 +1,58 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.Part;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelColumnFilter {
+	 public void filterExcel(InputStream fileContent) {		 	
+		 	LocalDate currentDate = LocalDate.now();
+//	        String inputFile = "C:\\Users\\PC-1\\eclipse-workspace\\TEst\\target\\Octane_defects_filtered_27_6_2023_11_23_08_am.xlsx";
+	        String outputFile = "C:\\Users\\PC-1\\eclipse-workspace\\TEst\\target\\DefectAgeing"+currentDate+".xlsx";
+	        String sheetName = "Data";
+	        int filterColumnIndex = 6; // Assuming the filter column is the third column (0-based index)
+	        List<String> filterValues = Arrays.asList("Deferred", "Fixed", "In Progress", "New", "Review"); // Filter values
+	        try (XSSFWorkbook workbook = new XSSFWorkbook(fileContent)) {
+	            XSSFSheet sheet = workbook.getSheet(sheetName);
+
+	            // Iterate through the rows and apply the filter
+	            Iterator<Row> rowIterator = sheet.iterator();
+	            while (rowIterator.hasNext()) {
+	                Row row = rowIterator.next();
+	                Cell cell = row.getCell(filterColumnIndex);
+
+	                // Check if it is the header row
+	                if (row.getRowNum() == 0) {
+	                    //Un-hide the header row
+	                    row.setZeroHeight(false);
+	                } else {
+	                    if (cell != null && filterValues.contains(cell.getStringCellValue())) {
+	                        // Keep the row
+	                        row.setZeroHeight(false);
+	                    } else {
+	                        // Hide the row
+	                        row.setZeroHeight(true);
+	                    }
+	                }
+	            }
+
+	            // Save the filtered data to a new workbook
+	            try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+	                workbook.write(fos);
+	                System.out.println("Filtered data saved successfully to " + outputFile);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
